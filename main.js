952fc7130920110f3f83cd356f1f7658c78f8349
Igor <ik@reclamare.ua>
2015-11-06 18:59:22 +0200

    Almost done

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
+
 jQuery(document).ready(function(){
 
     $('input').on("change",function(){
-        console.log($('input[type=file]')[0].files.length);
-        var filesArray = $('input[type=file]')[0].files;
-        console.log(filesArray);
-        filesArray.each(function(i,elem){
-           //var name = elem.name;
-           // console.log(name);
-        });
+        var $filesAmount = $('input[type=file]')[0].files.length;
+        //console.log($filesAmount);
+        var $filesArray = $('input[type=file]')[0].files;
+        //console.log(filesArray);
+        if($filesAmount > 0){
+            for(key in $filesArray){
+                //console.log(filesArray[key]);
+                if(typeof($filesArray[key]) === "object"){
+                    console.log($filesArray[key].name);
+                    $('#first-child').after('<p>'+ $filesArray[key].name +'<a href="#" id="'+'item-'+key+'"><i class="fa fa-times"></i></a></p>');
+                }
+            }
+        }
+    });
+
+
+    $('body').on("click", "#item-0",function(){
+        //alert('Hello!');
+        console.log('item-0 click');
     });
 });
